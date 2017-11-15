9fd2d18987507c7919f4c907091946e6f11ab50b
David Konsumer <konsumer@jetboystudio.com>
2017-11-14 21:04:27 -0800

    bad lang selector

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const visit = require('unist-util-visit')
 
 module.exports = ({ markdownAST }, { language = 'mermaid' } = {}) => {
   visit(markdownAST, 'code', node => {
-    let language = (node.lang || '').toLowerCase()
-    if (node.lang === language) {
+    let lang = (node.lang || '').toLowerCase()
+    if (lang === language) {
       node.type = 'html'
       node.value = `<div class="mermaid">${node.value}</div>`
     }
