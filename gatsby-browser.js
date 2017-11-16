09ddfc3382bdb98f3db46d55f922ccb5f0ecddd3
David Konsumer <konsumer@jetboystudio.com>
2017-11-16 01:18:45 -0800

    hack to make graphs update

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,13 +4,8 @@ exports.onInitialClientRender = (n, config) => {
   const s = document.createElement('script')
   s.setAttribute('src', 'https://unpkg.com/mermaid@7.1.0/dist/mermaid.min.js')
   document.head.appendChild(s)
-}
-
-exports.onRouteUpdate = ({ location }) => {
-  // refresh on nextTick
-  setTimeout(() => {
-    if (window.mermaid) {
-      window.mermaid.init(undefined, document.getElementsByClassName('mermaid'))
-    }
-  }, 100)
+  // XXX: ugly hack because onRouteUpdate doesn't know when the react is done
+  setInterval(() => {
+    window.mermaid.init(undefined, document.getElementsByClassName('mermaid'))
+  }, 200)
 }
