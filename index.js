fd07bc0843d45ef958c6bad455a4d8e4d4b1ddc9
David Konsumer <konsumer@jetboystudio.com>
2021-04-01 17:36:09 -0700

    let never changes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const visit = require('unist-util-visit')
 
 module.exports = ({ markdownAST }, { language = 'mermaid', theme = 'default' } = {}) => {
   visit(markdownAST, 'code', node => {
-    let lang = (node.lang || '').toLowerCase()
+    const lang = (node.lang || '').toLowerCase()
     if (lang === language) {
       node.type = 'html'
       node.value = `<div class="mermaid">${node.value}</div>`
