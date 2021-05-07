4d74b8ebf8fb155c9a608d6bd5f72a34056bd60a
Joe Wood <joewood@live.com>
2021-05-07 09:28:20 -0400

    Update Mermaid to v. 8.8.0

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,7 +2,7 @@ exports.onInitialClientRender = function(n, config) {
   window.mermaid_config = { theme: config.theme || 'default', startOnLoad: true }
   // load initial script
   const s = document.createElement('script')
-  s.setAttribute('src', 'https://unpkg.com/mermaid@8.5.2/dist/mermaid.min.js')
+  s.setAttribute('src', 'https://unpkg.com/mermaid@8.8.0/dist/mermaid.min.js')
   document.head.appendChild(s)
   // XXX: ugly hack because onRouteUpdate doesn't know when the react is done
   const init = () => {
