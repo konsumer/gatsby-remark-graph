99f99abb39f98145c3be30e9810a3f215634ee17
David Konsumer <konsumer@jetboystudio.com>
2021-05-07 18:27:58 -0700

    minor style tweak

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,13 +1,15 @@
-exports.onInitialClientRender = function(n, config) {
+exports.onInitialClientRender = function (n, config) {
   window.mermaid_config = { theme: config.theme || 'default', startOnLoad: true }
+
   // load initial script
   const s = document.createElement('script')
   s.setAttribute('src', 'https://unpkg.com/mermaid@8.8.0/dist/mermaid.min.js')
   document.head.appendChild(s)
+
   // XXX: ugly hack because onRouteUpdate doesn't know when the react is done
   const init = () => {
-    if(window.mermaid) window.mermaid.init(undefined, document.getElementsByClassName('mermaid'));
+    if (window.mermaid) window.mermaid.init(undefined, document.getElementsByClassName('mermaid'))
     else setInterval(init, 200)
   }
-  init();
+  init()
 }
